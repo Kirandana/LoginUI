d2553bdc85289561c76d84633c37f7e2f2ee12f5
Kirandana Arkanalla <88313502+Kirandana@users.noreply.github.com>
2024-11-12 16:56:25 +0700

    Update LoginUI.java
    Better UI Design

--- LoginUI.java
+++ LoginUI.java
@@ -1,17 +1,19 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
 
 public class LoginUI implements ActionListener {
 
     JFrame frame = new JFrame();
-    JLabel header;
+    JLabel header, reset;
     JTextField username;
     JTextField password;
     JButton login = new JButton();
 
     Font myFont = new Font("Arial", Font.PLAIN, 12);
     Font headerFont = new Font("Arial", Font.BOLD, 20);
+    Font resetFont = new Font("Arial", Font.PLAIN, 10);
 
     LoginUI() {
 
@@ -27,17 +29,28 @@ public class LoginUI implements ActionListener {
         username = new JTextField("Username");
         username.setBounds(45, 105, 200, 25);
         username.setFont(myFont);
+        username.setBorder(BorderFactory.createEmptyBorder()); // Removes outline
 
         password = new JTextField("Password");
         password.setBounds(45, 135, 200, 25);
         password.setFont(myFont);
+        password.setBorder(BorderFactory.createEmptyBorder());
 
         login = new JButton("Sign In");
         login.setBounds(45, 165, 200, 25);
         login.setFont(myFont);
+        login.setBackground(Color.BLACK); // Color of the Button
+        login.setForeground(Color.WHITE); // Color of the text
+        login.setBorder(BorderFactory.createEmptyBorder());
         login.setFocusable(false);
         login.addActionListener(this);
 
+        reset = new JLabel("Forgot password?");
+        reset.setBounds(110, 193, 200, 25);
+        reset.setFont(resetFont);
+        reset.setForeground(Color.BLUE);
+
+        frame.add(reset);
         frame.add(header);
         frame.add(login);
         frame.add(password);
